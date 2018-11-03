3676c4d851ac7224edbc6ad68382116cc5f91b07
chitty <rychitty@icloud.com>
2018-11-03 13:53:55 -0500

    html submit added

--- api.js
+++ api.js
@@ -1,12 +1,22 @@
-
+var userSearch = $('#search').val();
 var APIkey = 'fd73ba1ec8f2480d9eed93acc6de82a8';
-var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + APIkey;
+var searchQueryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + APIkey + '&q=' + userSearch;
 
 $.ajax({
-    url: queryURL,
+    url: searchQueryURL,
     method: "GET"
 }).then(function(response){
 
-console.log(response);
+
+
+})
+
+$('.search-btn').on('click', function(){
+
+
+    var uSearch = $('#search').val();
+    console.log(userSearch);
+    console.log(uSearch);
+
 
 })
