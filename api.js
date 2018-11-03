6e9b93feaa43e2b45a490786a036573b36c6980c
chitty <rychitty@icloud.com>
2018-11-03 13:09:58 -0500

    added ajax call

--- api.js
+++ api.js
@@ -1,1 +1,12 @@
-console.log("test");
+
+var APIkey = 'fd73ba1ec8f2480d9eed93acc6de82a8';
+var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + APIkey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+
+console.log(response);
+
+})
